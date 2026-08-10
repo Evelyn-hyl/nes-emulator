1d91ea38aa8da5ac8954b42a7a0e673e59de6f84
Evelyn-hyl <evvvta@gmail.com>
2026-08-09 19:24:47 -0700

    feat(ppu): add PPU register skeleton with bitfield unions
    Add PPUCTRL/PPUMASK/PPUSTATUS/LoopyRegister bitfield unions and wire
    up cpu_read/cpu_write to handle -, with VRAM/scroll access
    still stubbed as TODOs. Hook PPU into NES.

--- include/nes.hpp
+++ include/nes.hpp
@@ -2,11 +2,13 @@
 #define NES_EMULATOR_HPP
 
 #include "./cpu.hpp"
+#include "ppu.hpp"
 
 
 class NES {
   private:
     CPU cpu;
+    PPU ppu;
   
 };
 
--- src/ppu/ppu.cpp
+++ src/ppu/ppu.cpp
@@ -1,1 +1,54 @@
 #include "ppu.hpp"
+
+uint8_t PPU::cpu_read(uint16_t addr) {
+    switch(addr & 0x0007) {
+        case 0x0002: // $2002 PPUSTATUS
+            uint8_t status = (ppu_status_.reg & 0xE0) | (ppu_status_.open_bus_data & 0x1F);
+
+            ppu_status_.vblank = 0;
+            w_ = false;
+
+            return status;
+        
+        case 0x0004: // $2004 OAMDATA
+            return oam_[oam_addr_];
+
+        case 0x0007: // $2007 PPUDATA (VRAM Read)
+            return ppu_read_buffer_; // [TODO]: placeholder
+
+    }
+}
+
+void PPU::cpu_write(uint16_t addr, uint8_t data) {
+    switch(addr & 0x0007) {
+        case 0x0000: // $2000 PPUCTRL
+            ppu_ctrl_.reg = data;
+            break;
+
+        case 0x0001: // $2001 PPUMASK
+            ppu_mask_.reg = data;
+            break;
+
+        case 0x0003: // $2003 OAMADDR
+            oam_addr_ = data;
+            break;
+
+        case 0x0004: // $2004 OAMDATA
+            oam_[oam_addr_] = data;
+            oam_addr_++;
+            break;
+        
+        case 0x0005: // $2005 PPUSCROLL
+            // [TODO]: placeholder
+            break;
+        
+        case 0x0006: // $2006 PPUADDR
+            // [TODO]: placeholder
+            break;
+
+        case 0x0007: // $2007 PPUDATA (VRAM Write)
+            // [TODO]: placeholder
+            break;
+    }
+
+}
